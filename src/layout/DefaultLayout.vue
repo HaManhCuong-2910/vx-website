b9d337365aa6d3645fae5f3d23cd2a3cfafb9398
cuonghm <cuonghamanhcuong12@gmail.com>
2023-07-06 08:59:29 +0700

    dsadasda

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -7,7 +7,7 @@
 
 <style scoped lang="scss"></style>
 
-<script setup lang="ts">
+<script setup>
 import HeaderComponent from '@/components/layout/default/HeaderComponent.vue'
 import FooterComponent from '@/components/layout/default/FooterComponent.vue'
 import BackToTopButtonComponent from '@/components/common/BackToTopButtonComponent.vue'
